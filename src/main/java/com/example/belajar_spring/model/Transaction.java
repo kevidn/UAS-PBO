2edc5a568f99fdb3a41270f9c5360540856c205c
kevidn <kevindzakyh25@gmail.com>
2025-05-20 19:47:35 +0700

    feat: transaction 1

---
+++ src/main/java/com/example/belajar_spring/model/Transaction.java
@@ -0,0 +1,63 @@
+package com.example.belajar_spring.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "phone_id")
+    private Phone phone;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User buyer;
+
+    private LocalDateTime transactionDate;
+
+    public Transaction() {}
+
+    public Transaction(Phone phone, User buyer, LocalDateTime transactionDate) {
+        this.phone = phone;
+        this.buyer = buyer;
+        this.transactionDate = transactionDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+}
